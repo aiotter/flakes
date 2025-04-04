284dc02b6166896507cab225b466123efc523dc5
aiotter <git@aiotter.com>
2025-04-04 19:00:34 +0900

    feat: add package

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    packages = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        default = pkgs.buildNpmPackage {
+          pname = "reload";
+          version = "4.7.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "alallier";
+            repo = "reload";
+            rev = "3.3.0";
+            hash = "sha256-kbkd+TkTEcUOq62ageUMoHKE+UjDjicvXu4lkNsJVAQ=";
+          };
+          npmDepsHash = "sha256-x3uTFXQUNCbLITHrJZPpagiO9arAS6wu7+khvkZLcuw=";
+          dontNpmBuild = true;
+
+          meta = {
+            description = "node module to reload your browser when your code changes";
+            homepage = "https://github.com/alallier/reload";
+            license = pkgs.lib.licenses.mit;
+          };
+        };
+      });
+  };
+}
