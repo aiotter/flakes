c2ac061151a6f81746439a68848734a3fcb6a2cb
aiotter <git@aiotter.com>
2025-04-04 18:59:51 +0900

    feat(default): simplify

--- default/flake.nix
+++ default/flake.nix
@@ -1,25 +1,18 @@
 {
-  description = "";
-
   inputs = {
-    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      forSystems = systems: f:
-        builtins.foldl' (attrs: system: nixpkgs.lib.recursiveUpdate attrs (f system)) { } systems;
-      forAllSystems = forSystems nixpkgs.lib.systems.flakeExposed;
-    in
-    forAllSystems (system:
+  outputs = { self, nixpkgs }: {
+    packages = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
-        packages.${system}.default = pkgs.stdenv.mkDerivation rec {
+        default = pkgs.stdenv.mkDerivation {
           pname = "";
           version = "";
-          src = pkgs.fetchurl { };
         };
       });
+  };
 }
